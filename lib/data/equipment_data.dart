18f4027482e9f8e4423acb13391e6fdd1586f087
Dhananjani Kanchana <dhananjanikanchana@gmail.com>
2024-05-02 18:07:31 +0200

    create models,pages and data

---
+++ lib/data/equipment_data.dart
@@ -0,0 +1,48 @@
+import 'package:workout_planner/models/exercise_model.dart';
+
+class ExerciseData {
+  final List<Exercise> exerciseList = [
+    Exercise(
+      id: 0,
+      exerciseName: "Cobra",
+      exerciseImageUrl: "assets/exercise/cobra.png",
+      noOfMinutes: 5,
+      completed: false,
+    ),
+    Exercise(
+      id: 1,
+      exerciseName: "Downward Facing",
+      exerciseImageUrl: "assets/exercise/downward-facing.png",
+      noOfMinutes: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 2,
+      exerciseName: "Downward Facing",
+      exerciseImageUrl: "assets/exercise/downward-facing.png",
+      noOfMinutes: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 3,
+      exerciseName: "Downward Facing",
+      exerciseImageUrl: "assets/exercise/downward-facing.png",
+      noOfMinutes: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 4,
+      exerciseName: "Downward Facing",
+      exerciseImageUrl: "assets/exercise/downward-facing.png",
+      noOfMinutes: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 5,
+      exerciseName: "Downward Facing",
+      exerciseImageUrl: "assets/exercise/downward-facing.png",
+      noOfMinutes: 15,
+      completed: false,
+    ),
+  ];
+}
